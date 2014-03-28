e993c7698cb2f4bb6d465be434c6988ba62e99b9
addstar <adam@addstar.com.au>
2014-03-28 18:20:33 +1100

    Send plugin message (on "BungeeVoter" channel) when vote is received

--- src/au/com/addstar/bungeevoter/BungeeVoter.java
+++ src/au/com/addstar/bungeevoter/BungeeVoter.java
@@ -1,8 +1,13 @@
 package au.com.addstar.bungeevoter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 import com.vexsoftware.votifier.model.Vote;
 import com.vexsoftware.votifier.model.VotifierEvent;
 
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
@@ -13,6 +18,7 @@ public class BungeeVoter extends Plugin implements Listener {
 	@Override
 	public void onEnable() {
 		getProxy().getPluginManager().registerListener(this, this);
+		getProxy().registerChannel("BungeeVoter");
 	}
 	
 	@EventHandler
@@ -24,5 +30,25 @@ public class BungeeVoter extends Plugin implements Listener {
 				+ v.getAddress() + ", " 
 				+ v.getTimeStamp() + ", " 
 				+ v.getServiceName());
+		
+		ByteArrayOutputStream b = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(b);
+		
+		try {
+			out.writeUTF("vote");
+			out.writeUTF(v.getUsername());
+			out.writeUTF(v.getAddress());
+			out.writeUTF(v.getTimeStamp());
+			out.writeUTF(v.getServiceName());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+		
+		for(ServerInfo server : getProxy().getServers().values()) {
+			if(!server.getPlayers().isEmpty())
+				server.sendData("BungeeVoter", b.toByteArray());
+		}
 	}
 }
